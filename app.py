1d507d11e9c1fe5fa53ce6700b07bc8e35c73bf2
Muhammedhafis <129869634+Muhammedhafis@users.noreply.github.com>
2024-08-18 10:58:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Telegram bot token
-TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+TELEGRAM_BOT_TOKEN = '7282603200:AAEJnNY9Z-sQfBrrwzkroiY754NndaEPSlY'
 
 # Initialize the bot
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
